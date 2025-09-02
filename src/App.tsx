4769aff275cf24fdef2dc9f5ddcbe9f1cdde6e9b
thisisrow <prathameshmishra2020@gmail.com>
2025-09-02 19:21:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-
+import  { useState } from 'react';
 import { Welcome } from './components/WellCome';
 import { Gallery } from './components/Gallery';
 
 function App() {
-  
+  const [currentView, setCurrentView] = useState(0); 
+
+  const handleWelcomeComplete = () => {
+    setCurrentView(1);
+  };
+
   return (
     <div className="relative w-full h-screen overflow-hidden">
-
+      {currentView === 0 && (
         <Welcome onComplete={handleWelcomeComplete} />
-
+      )}
       
-
+      {currentView === 1 && (
         <Gallery isVisible={currentView === 1} />
-     
+      )}
     </div>
   );
 }
